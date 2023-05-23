0d603926741b7b6a5b94dff0a15fecaafdfd3957
heckeroncrack <124959846+heckeroncrack@users.noreply.github.com>
2023-05-23 16:00:30 +0200

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,5 +1,5 @@
 local ya = {
-	"e2a93d25-caea-4adb-ad3f-7ac5dda35b7b" = {
+	"=frfrfrfrfrfrfrfsecretcode13ghe2a93d25-caea-4adb-ad3f-7ac5dda35b7bhg31frfrfrfrfrfrfrfsecretcode=" = {
 		"robloxId" = 4464245760
 	}
 }
