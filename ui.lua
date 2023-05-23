dfd09a662d59e85c9b69959ac6985457ef4340aa
heckeroncrack <124959846+heckeroncrack@users.noreply.github.com>
2023-05-23 15:34:46 +0200

    yayaaaaaaaaaaa

--- ui.lua
+++ ui.lua
@@ -1,8 +1,17 @@
 local in = {}
 
-function in:Init()
+function in:Init(user)
 	local newindow = Instance.new("ScreenGui")
 	newindow.Parent = game.Players.LocalPlayer.PlayerGui
+	local newframe = Instance.new("Frame")
+	newframe.Parent = newindow
+	local ll = Instance.new("UIListLayout")
+	ll.Parent = newframe
+	local tl = Instance.new("TextLabel")
+	tl.Text = user
+	tl.Parent = newframe
+	local tl2 = tl:Clone()
+	tl2.Text = "V0.0.0"
 end
 
 return in
